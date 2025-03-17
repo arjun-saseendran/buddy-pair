d926c3c20c555c0e8e172e78b75874581dcc453d
Arjun Saseendran <arjunsaseendran@Arjuns-MacBook-Air.local>
2025-03-17 17:34:42 +0530

    update mobile responsive for invoice

--- src/components/Invoice/Invoice.jsx
+++ src/components/Invoice/Invoice.jsx
@@ -5,7 +5,7 @@ export const Invoice = () => {
   return (
     <div className="mx-20 ">
       <div className="mt-10 flex flex-col space-y-4 ">
-        <div className="flex justify-around items-center">
+        <div className="flex text-[6px] md:text-base gap-1 md:gap-0  md:flex-wrap-none md:justify-around items-center">
           <div>
             <input type="checkbox" />
           </div>
@@ -19,9 +19,9 @@ export const Invoice = () => {
           <div>CR</div>
           <div>Invoice</div>
         </div>
-        <hr className="border-1 border-gray-400 mt-2 overflow-x-auto" />
+        <hr className="border-1 border-gray-400 mt-2" />
 
-        <div className="flex justify-around items-center bg-white py-3 rounded-xl">
+        <div className="flex md:text-regular text-[6px] md:text-base gap-1 md:gap-0    md:justify-around items-center bg-white px-2 py-3 w-52 md:w-auto rounded-xl">
           <div>
             <input type="checkbox" />
           </div>
@@ -33,15 +33,17 @@ export const Invoice = () => {
           <div>0</div>
           <div>4954545</div>
           <div className="flex justify-around gap-4">
-          <div>
-            <FaFileInvoice />
-          </div>
-          <div>
-            <FaEdit />
-          </div>
+            <div>
+              <FaFileInvoice />
+            </div>
+            <div>
+              <FaEdit />
+            </div>
           </div>
         </div>
-        <div className="flex justify-around items-center bg-white py-3 rounded-xl">
+        <div className="flex justify-around 
+        px-2 md:text-regular text-[6px] md:text-base gap-1 md:gap-0 w-52 md:w-auto 
+        items-center md:flex-wrap-none bg-white py-3 rounded-xl">
           <div>
             <input type="checkbox" />
           </div>
@@ -53,15 +55,18 @@ export const Invoice = () => {
           <div>0</div>
           <div>4954545</div>
           <div className="flex justify-around gap-4">
-          <div>
-            <FaFileInvoice />
-          </div>
-          <div>
-            <FaEdit />
-          </div>
+            <div>
+              <FaFileInvoice />
+            </div>
+            <div>
+              <FaEdit />
+            </div>
           </div>
         </div>
-        <div className="flex justify-around items-center   bg-white py-3 rounded-xl">
+        <div className="flex justify-around items-center 
+        px-2 md:text-regular text-[6px] md:text-base gap-1 md:gap-0 w-52 md:w-auto 
+        
+        bg-white py-3 rounded-xl">
           <div>
             <input type="checkbox" />
           </div>
@@ -73,15 +78,17 @@ export const Invoice = () => {
           <div>0</div>
           <div>4954545</div>
           <div className="flex justify-around gap-4">
-          <div>
-            <FaFileInvoice />
-          </div>
-          <div>
-            <FaEdit />
-          </div>
+            <div>
+              <FaFileInvoice />
+            </div>
+            <div>
+              <FaEdit />
+            </div>
           </div>
         </div>
-        <div className="flex justify-around items-center   bg-white py-3 rounded-xl">
+        <div className="flex justify-around items-center 
+        px-2 md:text-regular text-[6px] md:text-base gap-1 md:gap-0 w-52 md:w-auto 
+           bg-white py-3 rounded-xl">
           <div>
             <input type="checkbox" />
           </div>
@@ -93,12 +100,12 @@ export const Invoice = () => {
           <div>0</div>
           <div>4954545</div>
           <div className="flex justify-around gap-4">
-          <div>
-            <FaFileInvoice />
-          </div>
-          <div>
-            <FaEdit />
-          </div>
+            <div>
+              <FaFileInvoice />
+            </div>
+            <div>
+              <FaEdit />
+            </div>
           </div>
         </div>
       </div>
