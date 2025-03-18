05348340f62db09dbb967c094968dcfc9c7f36a3
arjun-saseendran <arjunsaseendran@icloud.com>
2025-03-18 15:37:06 +0530

    update Invoice

--- src/components/Invoice/Invoice.jsx
+++ src/components/Invoice/Invoice.jsx
@@ -3,10 +3,10 @@ import { FaFileInvoice } from "react-icons/fa";
 
 export const Invoice = () => {
   return (
-    <div className="mx-20 mt-10 w-[790px] ">
-      <div className="mt-10 flex flex-col space-y-4">
-        <div className="flex text-[6px] md:text-base gap-1 md:gap-0  md:flex-wrap-none md:justify-around items-center">
-          <div>
+    <div className="mx-auto md:mx-20 mt-10 w-[790px] overflow-x-auto md:overflow-x-visible">
+      <div className="mt-10 flex flex-col space-y-4 min-w-[800px]">
+        <div className="flex gap-2 justify-around items-center">
+          <div className="ms-4">
             <input type="checkbox" />
           </div>
           <div>Sl.No</div>
@@ -14,14 +14,13 @@ export const Invoice = () => {
           <div>Invoice No</div>
           <div>Category</div>
           <div>Description/Remarks</div>
-
           <div>DR</div>
           <div>CR</div>
           <div>Invoice</div>
         </div>
         <hr className="border-1 border-gray-400 mt-2" />
 
-        <div className="flex md:text-regular text-[6px] md:text-base gap-1 md:gap-0    md:justify-around items-center bg-white px-2 py-3 w-52 md:w-auto rounded-xl">
+        <div className="flex justify-around items-center bg-white px-2 py-3 rounded-xl min-w-[800px]">
           <div>
             <input type="checkbox" />
           </div>
@@ -41,36 +40,8 @@ export const Invoice = () => {
             </div>
           </div>
         </div>
-        <div
-          className="flex justify-around
-        px-2 md:text-regular text-[6px] md:text-base gap-1 md:gap-0 w-52 md:w-auto
-        items-center md:flex-wrap-none bg-white py-3 rounded-xl"
-        >
-          <div>
-            <input type="checkbox" />
-          </div>
-          <div>1</div>
-          <div>xx/xx/xxxx</div>
-          <div>4954545</div>
-          <div>Other</div>
-          <div>Paid cash</div>
-          <div>0</div>
-          <div>4954545</div>
-          <div className="flex justify-around gap-4">
-            <div>
-              <FaFileInvoice />
-            </div>
-            <div>
-              <FaEdit />
-            </div>
-          </div>
-        </div>
-        <div
-          className="flex justify-around items-center
-        px-2 md:text-regular text-[6px] md:text-base gap-1 md:gap-0 w-52 md:w-auto
 
-        bg-white py-3 rounded-xl"
-        >
+        <div className="flex justify-around items-center bg-white px-2 py-3 rounded-xl min-w-[800px]">
           <div>
             <input type="checkbox" />
           </div>
@@ -90,11 +61,29 @@ export const Invoice = () => {
             </div>
           </div>
         </div>
-        <div
-          className="flex justify-around items-center
-        px-2 md:text-regular text-[6px] md:text-base gap-1 md:gap-0 w-52 md:w-auto
-           bg-white py-3 rounded-xl"
-        >
+
+        <div className="flex justify-around items-center bg-white px-2 py-3 rounded-xl min-w-[800px]">
+          <div>
+            <input type="checkbox" />
+          </div>
+          <div>1</div>
+          <div>xx/xx/xxxx</div>
+          <div>4954545</div>
+          <div>Other</div>
+          <div>Paid cash</div>
+          <div>0</div>
+          <div>4954545</div>
+          <div className="flex justify-around gap-4">
+            <div>
+              <FaFileInvoice />
+            </div>
+            <div>
+              <FaEdit />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-around items-center bg-white px-2 py-3 rounded-xl min-w-[800px]">
           <div>
             <input type="checkbox" />
           </div>
